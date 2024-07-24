d9e7e7daa7f1b9d5b1fcbb08cc171cb05455aadc
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-07-23 21:08:44 -0700

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        if self.full() == True:
+            print("Stack is already full. Cannot perform push operation.")
+        else: 
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        if target in self.items:
+            reversed_list = self.items[::-1]
+            return reversed_list.index(target)
+        else: 
+            return -1
